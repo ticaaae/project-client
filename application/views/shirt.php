3486fcfaadb23a58c00aac32390d214203d8feab
NirmalaPutri <nirmala.putri@mhs.itenas.ac.id>
2023-05-22 01:08:47 +0700

    'commit4'

--- application/views/shirt.php
+++ application/views/shirt.php
@@ -1,6 +1,6 @@
 <div class="content">
     <h2 class="intro-y text-lg font-medium mt-10">
-        Product List
+        Price List
     </h2>
     <div class="grid grid-cols-12 gap-6 mt-5">
         <div class="intro-y col-span-12 flex flex-wrap sm:flex-nowrap items-center mt-2">
